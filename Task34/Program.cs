9bea33697e38856fc935f95a4d6a26225f402920
vlad <vladislavludinov3006@gmail.com>
2023-01-20 20:00:25 +0300

    bug fix

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -3,12 +3,14 @@
 Write("Введите размер массива:");
 int size = int.Parse(ReadLine()!);
 
-int[] getArray(int [] sz)
+int[] getArray(int  sz)
 {
     Random rnd = new Random();
     int [] arr = new int [sz];
     for (int i = 0; i < size; i++)
     {
-        sz[i] = rnd.Next(100,1000);
+        arr[i] = rnd.Next(100,1000);
     }
+    return arr;
 }
+
