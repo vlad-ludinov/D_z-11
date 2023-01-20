3a1529e020042d0223e037f76f56fd572f9b45e5
vlad <vladislavludinov3006@gmail.com>
2023-01-20 19:58:50 +0300

    method creatig array added

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -3,3 +3,12 @@
 Write("Введите размер массива:");
 int size = int.Parse(ReadLine()!);
 
+int[] getArray(int [] sz)
+{
+    Random rnd = new Random();
+    int [] arr = new int [sz];
+    for (int i = 0; i < size; i++)
+    {
+        sz[i] = rnd.Next(100,1000);
+    }
+}
