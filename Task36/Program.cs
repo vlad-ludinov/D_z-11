16d238ed5e3b4ae179589db2e6773dfc47d8e5b8
vlad <vladislavludinov3006@gmail.com>
2023-01-20 20:53:57 +0300

    input massive's parametres added

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,5 +1,9 @@
 ﻿using static System.Console;
 
+Write("Введите размер, минимальное и максимальное значения массива через запятую: ");
+string [] parametres = ReadLine()!.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+
 int [] getArray(int sz, int minVl, int maxVl);
 {
     Random rnd = new Random ();
